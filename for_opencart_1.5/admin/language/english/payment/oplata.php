d6830e0cc100a52c177e0a22f452b6963d17aa17
Andrii Sydorenko <dev.sidorenko@gmail.com>
2024-05-21 12:04:15 +0300

    init

---
+++ for_opencart_1.5/admin/language/english/payment/oplata.php
@@ -0,0 +1,38 @@
+<?php
+// Heading
+$_['heading_title']      = 'Alliance bank';
+
+// Text 
+$_['text_payment']       = 'Payment';
+$_['text_oplata']        = '<a onclick="window.open(\'https://fondy.eu\');"><img src="view/image/payment/oplata.png" alt="Alliance bank" title="Alliance bank" style="border: 1px solid #EEEEEE;" /></a>';
+$_['text_success']       = 'Settings updated';   
+$_['text_pay']           = 'Alliance bank';
+$_['text_card']          = 'Visa/MasterCard';
+
+$_['text_response_description']               = 'Error description:';
+$_['text_oplata_order_status']                = 'Order status:';
+$_['text_response_code']                      = 'Error code:';
+// Entry
+
+$_['entry_merchant']     = 'Merchant ID:';
+$_['entry_secretkey']    = 'Secret key:';
+
+$_['entry_order_status'] = 'Order status after payment:';
+$_['entry_order_process_status'] = 'Order status in progress:';
+$_['entry_order_status_cancelled']    =  'Order status in cancelled:';
+$_['entry_currency']     = 'Currency:';
+$_['entry_process_payment_type'] = 'Payment integration type:';
+$_['entry_redirect'] = 'Redirect to Alliance bank';
+$_['entry_built_in_checkout'] = 'Built in checkout';
+$_['entry_backref']      = 'Back refference :<br /><span class="help">http://{your_domain}/index.php?route=payment/oplata/response</span>';
+$_['entry_server_back']  = 'Server back refference :<br /><span class="help">http://{your_domain}/index.php?route=payment/oplata/callback</span>';
+$_['entry_language']     = 'Payment page language :<br /><span class="help">default : RU </span>';
+
+$_['entry_status']       = 'Status:';
+$_['entry_sort_order']   = 'Ordering:';
+
+// Error
+$_['error_permission']   = "You haven't permission !";
+$_['error_merchant']     = 'Merchant ID are incorrect!';
+$_['error_secretkey']    = 'Secret key are empty!';
+?>
---
+++ for_opencart_2.3.x/README.md
@@ -0,0 +1,22 @@
+# UK
+
+Встановлення
+-------------
+1. [Завантажити модуль](https://github.com/cloudipsp/opencart-all-versions/raw/master/for_opencart_2.3.x/fondy.ocmod.zip "Завантажити") і встановити його через установку модулів opencart (``Навігація/Модулі/Установка модулів``).
+   >**або** [завантажити репозиторій](https://github.com/cloudipsp/opencart-v3/archive/main.zip) та скопіювати все з каталогу `upload` в корінь сайту (якщо встановлена стороння тема - каталог `payment` з ```/catalog/view/theme/default/template/payment/``` скопіювати в каталог з темою)
+2. У списку модулів (``Навігація/Модулі/Модулі/Оплата``) знайти спосіб оплати ***Alliance bank*** та натиснути кнопку ***"Інсталювати"***.
+3. Натиснути кнопку ***"Редагувати"*** та заповнити необхідні поля.
+4. Зберегти зміни.
+
+-------------
+
+# EN
+
+Installation
+-------------
+1. [Download the module](https://github.com/cloudipsp/opencart-all-versions/raw/master/for_opencart_2.3.x/fondy.ocmod.zip "Download")  and install it through the opencart module installer (``Navigation/Extenson/Installer``)
+    > **or** copy all the contents in the `upload` directory to the root directory site of your shop (if installed custom theme - folder  `payment` from ```/catalog/view/theme/default/template/payment/``` copy in folder of your custom theme)
+2. In list of all the available payment methods ``Navigation/Extenson/Extenson/Paymens`` find payment method ***Alliance bank*** and click ***"Install"*** button.
+3. Click the ***"Edit"*** button and fill in the required fields.
+4. Save the changes.
+-------------
